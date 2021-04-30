693a4b08246081fc9c8e5e20eebccc44a38aa665
Aryan <aryangangwar000@gmail.com>
2021-05-01 01:51:51 +0530

    feat: VALIDATION

--- app/src/main/java/com/example/accime/register.java
+++ app/src/main/java/com/example/accime/register.java
@@ -2,7 +2,13 @@ package com.example.accime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class register extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        View signup = findViewById(R.id.textView10);
+        EditText email = (EditText) findViewById(R.id.et_email);
+        EditText password = (EditText) findViewById(R.id.et_password);
+        View login = findViewById(R.id.button2);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email.getText().toString().isEmpty()  ) {
+                    Toast.makeText(getApplicationContext(), "PLEASE ENTER EMAIL", Toast.LENGTH_SHORT).show();
+                }else if(password.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "PLEASE ENTER PASSWORD", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "SUCCESS", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+                startActivity(new Intent(register.this, registration.class));
+                finish();
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/accime/registration.java
+++ app/src/main/java/com/example/accime/registration.java
@@ -2,7 +2,11 @@ package com.example.accime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class registration extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        View login = findViewById(R.id.tv_Login);
+        View register = findViewById(R.id.bt_register);
+        EditText firstname = (EditText) findViewById(R.id.et_firstName);
+        EditText lastname = (EditText) findViewById(R.id.et_lastName);
+        EditText email_reg = (EditText) findViewById(R.id.et_email);
+        EditText phone = (EditText) findViewById(R.id.et_phone);
+        EditText aadhar = (EditText) findViewById(R.id.et_aadhar);
+        EditText password = (EditText) findViewById(R.id.et_password);
+        EditText ConfirmPass = (EditText) findViewById(R.id.et_confirmppassword_register3);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(registration.this,register.class));
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        if (firstname.getText().toString().isEmpty()  ) {
+            Toast.makeText(getApplicationContext(), "PLEASE ENTER FIRST NAME", Toast.LENGTH_SHORT).show();
+        }else if(lastname.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "PLEASE ENTER LAST NAME", Toast.LENGTH_SHORT).show();
+        }else if(email_reg.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "PLEASE ENTER EMAIL", Toast.LENGTH_SHORT).show();
+        }else if(phone.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "PLEASE ENTER PHONE", Toast.LENGTH_SHORT).show();
+        }else if(aadhar.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "PLEASE ENTER YOUR AADHAR NUMBER", Toast.LENGTH_SHORT).show();
+        }else if(password.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "PLEASE ENTER PASSWORD", Toast.LENGTH_SHORT).show();
+        }else if(ConfirmPass.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "PLEASE CONFIRM PASSWORD", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "SUCCESS", Toast.LENGTH_SHORT).show();
+        }
     }
 }
